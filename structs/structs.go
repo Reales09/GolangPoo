295a92b27af14aec31cbb79fad9fc87f1ac77fe4
Reales Myles <rclarence123@gmail.com>
2023-05-01 10:33:26 -0500

    Metodos

--- structs/structs.go
+++ structs/structs.go
@@ -9,15 +9,34 @@ type Persona struct {
 	edad   int
 }
 
+//Metodos
+
+func (p *Persona) imprimir() {
+	fmt.Printf("Nombre: %s, Edad: %d ", p.nombre, p.edad)
+
+}
+
+func (p *Persona) editarNombre(nuevoNombre string) {
+	p.nombre = nuevoNombre
+}
+
 func main() {
 
-	persona1 := Persona{"Juan", 20}
-	fmt.Println(persona1)
-	persona1.nombre = "Reales"
+	persona1 := Persona{"Reales", 20}
+	// fmt.Println(persona1)
 
-	fmt.Println(persona1)
+	persona1.imprimir()
+	// fmt.Println(persona1)
+	persona1.editarNombre("Pedro")
+
+	persona1.imprimir()
 
 	persona2 := Persona{nombre: "Adriana", edad: 20}
 
-	fmt.Println(persona2)
+	// fmt.Println(persona2)
+
+	persona2.imprimir()
+
+	persona2.editarNombre("Maria")
+	persona2.imprimir()
 }
