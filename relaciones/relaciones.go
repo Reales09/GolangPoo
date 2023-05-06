c470acd1aa9451ae47e08598ad149b08eefe00ad
Reales <rclarence123@gmail.com>
2023-05-05 23:43:04 -0500

    De uno a muchos

--- relaciones/relaciones.go
+++ relaciones/relaciones.go
@@ -13,27 +13,61 @@ type Student struct {
 	codigo string
 }
 
+// Relacion uno a muchos
+
+type Curso struct {
+	titulo string
+	videos []Video
+}
+
+type Video struct {
+	titulo string
+	curso  Curso
+}
+
 func main() {
 
-	alex := User{
-		nombre: "Alexys",
-		email:  "djkdlk@gmail.com",
-		activo: true,
+	// Relacion uno a uno
+
+	/*
+		alex := User{
+			nombre: "Alexys",
+			email:  "djkdlk@gmail.com",
+			activo: true,
+		}
+
+		reales := User{
+			nombre: "reales",
+			email:  "reales@gmail.com",
+			activo: true,
+		}
+
+		realesStudent := Student{
+			user:   reales,
+			codigo: "0001",
+		}
+
+		fmt.Println(alex)
+		fmt.Println(reales)
+		fmt.Println(realesStudent.user.nombre)
+	*/
+
+	// Relacion uno a muchos
+	video1 := Video{titulo: "01-Introduccion"}
+	video2 := Video{titulo: "02-Instalacion"}
+
+	curso := Curso{
+		titulo: "Curso de Go",
+		videos: []Video{video1, video2},
 	}
 
-	reales := User{
-		nombre: "reales",
-		email:  "reales@gmail.com",
-		activo: true,
-	}
+	video1.curso = curso
+	video2.curso = curso
 
-	realesStudent := Student{
-		user:   reales,
-		codigo: "0001",
-	}
+	fmt.Println(video1.curso.titulo)
 
-	fmt.Println(alex)
-	fmt.Println(reales)
-	fmt.Println(realesStudent.user.nombre)
+	for _, video := range curso.videos {
+		fmt.Println(video.titulo)
+	}
 
 }
