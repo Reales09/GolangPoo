cb3f81fb95fc4954a9ff592882fd31ef2bff6943
Reales Myles <rclarence123@gmail.com>
2023-05-06 23:37:29 -0500

    Hallar el area y perimetro de un cuadrado circulo con interfaces.

--- 04-interfaces/interfaces.go
+++ 04-interfaces/interfaces.go
@@ -2,9 +2,39 @@ package main
 
 import "fmt"
 
+type Area interface {
+	area() float64
+	perimetro() float64
+}
+
+type cuadrado struct {
+	lado float64
+}
+
+type circulo struct {
+	radio float64
+}
+
 type Animal interface {
 	mover() string
 }
+
+func (a *cuadrado) area() float64 {
+	return a.lado * a.lado
+}
+
+func (a *cuadrado) perimetro() float64 {
+	return 2*a.lado + 2*a.lado
+}
+
+func (a *circulo) area() float64 {
+	return a.radio * a.radio * 3.14
+}
+
+func (c *circulo) perimetro() float64 {
+	return 2 * 3.14 * c.radio
+}
+
 type Perro struct{}
 type Pez struct{}
 type Pajaro struct{}
@@ -26,6 +56,18 @@ func moverAnimal(animal Animal) {
 	fmt.Println(animal.mover())
 }
 
+func printArea(area Area) {
+	fmt.Printf("El area del cuadrado es: %v \n", area.area())
+	fmt.Printf("El perimetro de un cuadrado es: %v \n", area.perimetro())
+
+}
+
+func printAreaCirculo(area Area) {
+	fmt.Printf("El perimetro del cirsulo es: %v \n", area.area())
+	fmt.Printf("El perimetro del circulo es: %v \n", area.perimetro())
+
+}
+
 func main() {
 	perro := Perro{}
 	moverAnimal(&perro)
@@ -35,4 +77,11 @@ func main() {
 
 	pajaro := Pajaro{}
 	moverAnimal(&pajaro)
+
+	areaCuadradro := cuadrado{lado: 10}
+	printArea(&areaCuadradro)
+
+	areaCirculo := circulo{radio: 10}
+	printAreaCirculo(&areaCirculo)
+
 }
